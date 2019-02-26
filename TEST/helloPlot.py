9061fadb87280213f9aae5a449db99393f69a9c2
Lin Renwen <linrenwen@gmail.com>
2019-02-26 15:50:01 +0800

    2019-2-26 15:49:59

--- TEST/helloPlot.py
+++ TEST/helloPlot.py
@@ -1,16 +1,21 @@
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
+from numpy import pi, arange, sin
 
 x=[0,1]
 y=[0,1]
+
+t = arange(0.0, 10*pi, 0.1)
+yt = sin(2*pi*t)*t
+
 print("hello")
 
 
 csfont = {'fontname':'Times New Roman'}
 plt.figure()
 plt.rcParams["font.family"] = "Times New Roman"
-plt.title('New Tile')
-plt.plot(x,y)
+plt.title('New Title')
+plt.plot(t,yt,'.-')
 plt.savefig("easyplot.svg")
 # plt.savefig("easyplot.emf")
 
