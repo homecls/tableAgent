cdef37df761bd435626a00505a69ea825acd6ae9
Lin Renwen <linrenwen@gmail.com>
2019-02-26 13:14:14 +0800

    2019-2-26 13:14:12

--- TEST/helloPlot.py
+++ TEST/helloPlot.py
@@ -3,6 +3,19 @@ import matplotlib.pyplot as plt
 
 x=[0,1]
 y=[0,1]
+print("hello")
+
+
+csfont = {'fontname':'Times New Roman'}
 plt.figure()
+plt.rcParams["font.family"] = "Times New Roman"
+plt.title('New Tile')
 plt.plot(x,y)
-# plt.savefig("easyplot.jpg")
+plt.savefig("easyplot.svg")
+# plt.savefig("easyplot.emf")
+
+
+
+## write your code related to basemap here
+
+## plt.show()
