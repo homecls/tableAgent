8265c3fb2c026dfe7356e17ce8f295e3a110f29a
Lin Renwen <linrenwen@gmail.com>
2019-03-26 19:53:50 +0800

    2019-3-26 19:53:49

--- TEST/zipMfileForshare_test.m
+++ TEST/zipMfileForshare_test.m
@@ -1,5 +1,6 @@
 % Author: linrenwen@gmail.com
 % 2019/02/24
+% 缺失两个文件，需要补充：mysin.m  DataTemp.xlsb, 并拷贝如.zip文件
 
 clc; clear; close all; %fclose all
 P = addpathprj('tableAgent');
@@ -7,3 +8,4 @@ cdmfile(mfilename);
 tic
 [pfZip,pfList,pfMain,pfClass] = packageMfile('tableAgent_test.m');
 toc
+
